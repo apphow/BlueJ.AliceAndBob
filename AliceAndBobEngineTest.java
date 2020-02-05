ff98039789b2399e045fc9765d82a268a5376974
April Howard <ahoward@zipcodes-MacBook-Pro-5.local>
2020-02-05 18:04:24 -0500

    AliceAndBobEngineTest

--- AliceAndBobEngineTest.java
+++ AliceAndBobEngineTest.java
@@ -8,8 +8,8 @@ import org.junit.Test;
 /**
  * The test class AliceAndBobEngineTest.
  *
- * @author  (your name)
- * @version (a version number or a date)
+ * @author  (April Howard)
+ * @version (02/05/2020)
  */
 public class AliceAndBobEngineTest
 {
